09a1f7da3a9afdd2497a27a681881cb7001313de
Niklas Leopold <niklas.leopold@verisure.com>
2023-09-22 08:07:21 +0200

    adding e2e

--- apps/replace-e2e/src/e2e/app.cy.ts
+++ apps/replace-e2e/src/e2e/app.cy.ts
@@ -8,6 +8,6 @@ describe('replace', () => {
     cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome replace');
+    getGreeting().contains('The amazing replace app');
   });
 });
--- apps/replace/src/app/app.tsx
+++ apps/replace/src/app/app.tsx
@@ -1,12 +1,11 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
 
-import NxWelcome from './nx-welcome';
 
 export function App() {
   return (
     <div>
-      <NxWelcome title="replace" />
+      <h1>The amazing replace app</h1>
     </div>
   );
 }
